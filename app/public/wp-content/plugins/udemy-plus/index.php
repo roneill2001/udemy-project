01f4d255aaa88e726da1966e5408f1f25506c8a7
roneill2001 <136326425+roneill2001@users.noreply.github.com>
2023-07-17 16:59:25 +0100

    Git Startup

---
+++ app/public/wp-content/plugins/udemy-plus/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:       Udemy Plus
+ * Plugin URI:        https://www.udemy.com
+ * Description:       This is a Udemy Plus plugin for adding blocks to a theme.
+ * Version:           1.0.0
+ * Requires at least: 5.9
+ * Requires PHP:      7.2
+ * Author:            Ross O'Neill
+ * Author URI:        https://www.udemy.com
+ * Text Domain:       udemy-plus
+ * Domain Path:       /languages
+ */
